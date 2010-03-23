9c80fd3bca8d02eaf8e0db319bed393e081b7a79
deaddivinity <deaddivinity@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-03-23 10:03:51 +0000

    Added a DataWriterViewHelper that works together with the extbase matching DataWriter (extjs-side). Using this DataWriter makes a store matching the parameter structure of extbase. This allows you to have actions in your controller, that receive modified domain objects from extjs store in cases like update or create.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mvc_extjs/branches/view@31373 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Classes/ViewHelpers/JsCode/DataWriterViewHelper.php
@@ -0,0 +1,137 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2010 Dennis Ahrens <dennis.ahrens@fh-hannover.de>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * View helper which allows you to include an ExtJS Store based on the object notation
+ * of a domain model
+ * Note: This feature is experimental!
+ * Note: You MUST wrap this Helper with <mvcextjs:be.moduleContainer>-Tags
+ *
+ * = Examples =
+ *
+ * <mvcextjs:be.moduleContainer pageTitle="foo" enableJumpToUrl="false" enableClickMenu="false" loadPrototype="true" loadScriptaculous="false" loadExtJs="true" loadExtJsTheme="false" extJsAdapter="prototype" enableExtJsDebug="true">
+ * 	<mvcextjs:Be.IncludeStore domainModel="yourModelName" actions="{read:'yourActionForFetchingTheRecords',update:'yourActionForUpdatingRecords'}" controller="yourController" extensionName="yourExtensionName" />
+ * </mvcextjs:be.moduleContainer>
+ *
+ * @category    ViewHelpers
+ * @package     TYPO3
+ * @subpackage  tx_mvcextjs
+ * @author      Dennis Ahrens <dennis.ahrens@fh-hannover.de>
+ * @license     http://www.gnu.org/copyleft/gpl.html
+ * @version     SVN: $Id: JsonWriterViewHelper.php 30242 2010-02-20 14:32:48Z xperseguers $
+ */
+class Tx_MvcExtjs_ViewHelpers_JsCode_DataWriterViewHelper extends Tx_MvcExtjs_ViewHelpers_JsCode_AbstractJavaScriptCodeViewHelper {
+
+	/**
+	 * @var Tx_MvcExtjs_CodeGeneration_JavaScript_ExtJS_ExtendClass
+	 */
+	protected $writer;
+
+	/**
+	 * @var Tx_MvcExtjs_CodeGeneration_JavaScript_ExtJS_Config
+	 */
+	protected $config;
+
+	/**
+	 * Initializes the ViewHelper
+	 * 
+	 * @see Classes/ViewHelpers/Be/Tx_MvcExtjs_ViewHelpers_Be_AbstractJavaScriptCodeViewHelper#initialize()
+	 */
+	public function initialize() {
+		parent::initialize();
+		$this->config = new Tx_MvcExtjs_CodeGeneration_JavaScript_ExtJS_Config();
+		$this->writer = new Tx_MvcExtjs_CodeGeneration_JavaScript_ExtJS_ExtendClass(
+			'defaultJsonReaderName',
+			'Ext.ux.TYPO3.MvcExtjs.DataWriter',
+			array(),
+			$this->config,
+			new Tx_MvcExtjs_CodeGeneration_JavaScript_Object(),
+			$this->extJsNamespace
+		);
+	}
+
+	/**
+	 * Renders the Code for a JsonReader build up on the data given by the domainModel
+	 * 
+	 * @param string $domainModel is used as variable name AND storeId for the generated store
+	 * @param string $extensionName the EXT where the domainModel is located
+	 * @param string $id choose a id for the created variable; default is $domainmodel . 'JsonReader'
+	 * @param boolean $encode
+	 * @param boolean $writeAllFields
+	 * @return void
+	 */
+	public function render($domainModel = NULL,
+						   $extensionName = NULL,
+						   $name = NULL,
+						   $objectName = NULL,
+						   $moduleName = NULL) {
+
+		if ($extensionName === NULL) {
+			$extensionName = $this->controllerContext->getRequest()->getControllerExtensionName();
+		}
+
+		$domainClassName = 'Tx_' . $extensionName . '_Domain_Model_' . $domainModel;
+			// Check if the given domain model class exists
+		if (!class_exists($domainClassName)) {
+			throw new Tx_Fluid_Exception('The Domain Model Class (' . $domainClassName . ') for the given domainModel (' . $domainModel . ') was not found', 1264069568);
+		}
+			// Build up and set the for the JS store variable
+		if ($name === NULL) {
+			$name = $domainModel . 'DataWriter';
+		}
+		if ($objectName === NULL) {
+			$objectName = $domainModel;
+			$objectName{0} = strtolower($objectName{0});
+		}
+		if ($moduleName === NULL) {
+			throw new Tx_MvcExtjs_ExtJs_Exception('If no module name is given the object cannot be mapped by extbase!',1269335971);
+		}
+		$extKey = 'tx_' . strtolower($extensionName);
+		
+		$this->writer->setName($name);
+			// Read the given config parameters into the Extjs Config Object
+		$this->config->set('objectName', $objectName)
+					 ->set('moduleName', $moduleName)
+					 ->set('extKey',$extKey);
+			// render the code
+		$this->injectJsCode();
+	}
+
+	/**
+	 * (non-PHPdoc)
+	 * @see Classes/ViewHelpers/JsCode/Tx_MvcExtjs_ViewHelpers_JsCode_AbstractJavaScriptCodeViewHelper#injectJsCode()
+	 */
+	public function injectJsCode() {
+			// Apply the configuration again
+		$this->writer->setConfig($this->config);
+			// Allow objects to be declared inside this viewhelper; they are rendered above
+		$this->renderChildren();
+			// Add the code and write it into the inline section in your HTML head
+		//$this->jsCode->addSnippet(new Tx_MvcExtjs_CodeGeneration_JavaScript_Snippet('Ext.ns("Ext.ux.TYPO3.MvcExtjs");'));
+		$this->jsCode->addSnippet($this->writer);
+		parent::injectJsCode();
+	}
+
+}
+?>
---
+++ Resources/Public/JavaScript/ux.TYPO3.MvcExtjs.DataWriter.js
@@ -0,0 +1,80 @@
+Ext.ns('Ext.ux.TYPO3.MvcExtjs');
+/**
+ * 
+ */
+Ext.ux.TYPO3.MvcExtjs.DataWriter = Ext.extend(Ext.data.DataWriter, {
+    
+	/**
+	 * the name of the variable on php side
+	 */
+	objectName : 'genre',
+	/**
+	 * the key of the module
+	 */
+	moduleName : 'user_MvcExtjsSamplesViewbased',
+	/**
+	 * name of the extension in format, that extbase sets in from of the module key
+	 * (for the expected parameter prefix)
+	 */
+	extKey: 'tx_mvcextjssamples',
+	
+	constructor : function(config){
+		Ext.ux.TYPO3.MvcExtjs.DataWriter.superclass.constructor.call(this, config);    
+	},
+	
+	/**
+	 * Final action of a write event.  Apply the written data-object to params.
+	 * 
+	 * TODO: support request hash generation as expected by extbase
+	 * 
+     * @param {String} action [Ext.data.Api.actions.create|read|update|destroy]
+     * @param {Record[]} rs
+     * @param {Object} http params
+     * @param {Object} data object populated according to DataReader meta-data "root" and "idProperty"
+     */
+	render : function(action, rs, params, data) {
+	 	Ext.iterate(data.data, function(key, value) {
+	 		if (key === 'uid') {
+	 			params[this.createParameterPrefix() + '[' + this.objectName + '][__identity]'] = value;
+	 		} else {
+	 			params[this.createParameterPrefix() + '[' + this.objectName + '][' + key + ']'] = value;
+	 		}
+ 		}, this);
+	},
+	    
+	/**
+	  * Implements abstract Ext.data.DataWriter#createRecord
+	  * @protected
+	  * @param {Ext.data.Record} rec
+	  * @return {Object}
+	  */
+	createRecord : function(rec) {
+	   	var data = this.toHash(rec);
+	    delete data[this.meta.idProperty];
+	    return data;
+	},
+	    
+	/**
+	 * Implements abstract Ext.data.DataWriter#updateRecord
+	 * @protected
+	 * @param {Ext.data.Record} rec
+	 * @return {Object}
+	 */
+	updateRecord : function(rec) {
+		return this.toHash(rec);
+	},
+	    
+	/**
+	 * Implements abstract Ext.data.DataWriter#destroyRecord
+	 * @protected
+	 * @param {Ext.data.Record} rec
+	 * @return {Object}
+	 */
+	destroyRecord : function(rec){
+		return rec.id;
+	},
+	
+	createParameterPrefix : function() {
+		return this.extKey + '_' + (this.moduleName+'').toLowerCase();
+	}
+});
